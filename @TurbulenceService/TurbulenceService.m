35beb42ab6519c8104570041117703674bc7b852
Edo Frederix <edofrederix@gmail.com>
2011-08-14 13:35:51 -0400

    Properly including the Matlab-Fast-SOAP package

--- @TurbulenceService/TurbulenceService.m
+++ @TurbulenceService/TurbulenceService.m
@@ -1,5 +1,21 @@
 function obj = TurbulenceService
 
+%
+a = dir;
+set = 0;
+for i = 1:numel(a)
+    if a(i).isdir && ~isempty(regexpi(a(i).name, '^matlab.fast.soap'))
+        addpath(a(i).name);
+        set = 1;
+        break;
+    end
+end
+
+if ~set
+    error('Could not find Matlab-Fast-SOAP package. Please place the Matlab-FAST-SOAP package in the root of this package.');
+end
+
+
 obj.endpoint = 'http://turbulence.pha.jhu.edu/service/turbulence.asmx';
 obj.wsdl = 'http://turbulence.pha.jhu.edu/service/turbulence.asmx?WSDL';
 
